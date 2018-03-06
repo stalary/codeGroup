cc1ad13df60d9e9d5e59f5124a5d3904e99d4dc1
stalary <452024236@qq.com>
2018-03-06 15:25:17 +0800

    ADD:init

---
+++ src/main/java/com/stalary/codegroup/CodegroupApplication.java
@@ -0,0 +1,12 @@
+package com.stalary.codegroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegroupApplication.class, args);
+	}
+}
